1647b5c22a1c4a4a21568600efbd5bdb455e1928
Angel Pashov <86370508+PashataDLG@users.noreply.github.com>
2022-07-20 17:20:39 +0300

    React router added

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,5 @@
+import { Routes, Route } from 'react-router-dom'
+
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 
@@ -9,9 +11,12 @@ function App() {
             <Header />
             {/* Main Content */}
             <main id="main-content">
-                <Home />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                </Routes>
+
             </main>
-            
+
             {/* Login Page ( Only for Guest users ) */}
             {/* <section id="login-page" className="auth">
                 <form id="login">
@@ -130,8 +135,8 @@ function App() {
                         <input className="btn submit" type="submit" defaultValue="Edit Game" />
                     </div>
                 </form>
-            </section> */}         
-       </div>
+            </section> */}
+        </div>
 
     );
 }
