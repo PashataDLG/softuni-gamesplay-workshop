8edd6f7cc13bd463a14f5e2e9d5d45e6308fde93
Angel Pashov <86370508+PashataDLG@users.noreply.github.com>
2022-07-20 20:26:00 +0300

    Dynamic details page rendering added

--- client/src/App.js
+++ client/src/App.js
@@ -37,7 +37,7 @@ function App() {
                     <Route path="/register" element={<Register />} />
                     <Route path="/create" element={<Create />} />
                     <Route path="/edit" element={<Edit />} />
-                    <Route path="/details" element={<Details />} />
+                    <Route path="/details/:gameId" element={<Details games={games} />} />
                 </Routes>
             </main>
 
