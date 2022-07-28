0700790fa7194257276b2b0e9d3489c1301a0157
Angel Pashov <86370508+PashataDLG@users.noreply.github.com>
2022-07-28 12:58:56 +0300

    Register Functionallity added

--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -1,9 +1,36 @@
-import { Link } from 'react-router-dom';
+import { useContext } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+
+import * as authService from '../../services/authService';
+import { AuthContext } from '../../contexts/authContext';
 
 const Register = () => {
+    const { userLogin } = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+
+        const {
+            email,
+            password,
+            rePassword
+        } = Object.fromEntries(new FormData(event.target));
+
+        if (password !== rePassword) {
+            return alert('Passwords must match');
+        }
+
+        authService.register(email, password)
+            .then(authData => {
+                userLogin(authData);
+                navigate('/');
+            });
+    };
+
     return (
         <section id="register-page" className="content auth">
-            <form id="register">
+            <form id="register" onSubmit={onSubmit}>
                 <div className="container">
                     <div className="brand-logo" />
                     <h1>Register</h1>
@@ -17,7 +44,7 @@ const Register = () => {
                     <label htmlFor="pass">Password:</label>
                     <input type="password" name="password" id="register-password" />
                     <label htmlFor="con-pass">Confirm Password:</label>
-                    <input type="password" name="confirm-password" id="confirm-password" />
+                    <input type="password" name="rePassword" id="confirm-password" />
                     <input className="btn submit" type="submit" defaultValue="Register" />
                     <p className="field">
                         <span>
--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -2,5 +2,9 @@ import * as request from "../util/requester";
 
 const baseUrl = 'http://localhost:3030'
 
-export const login = (email, password) => 
+export const login = (email, password) =>
     request.post(`${baseUrl}/users/login`, { email, password });
+
+
+export const register = (email, password) =>
+    request.post(`${baseUrl}/users/register`, { email, password });
